a3bebbcb2959484e806d9ea08d094ced4c96b941
Daemonlite <khemikal2016@gmail.com>
2023-12-26 09:17:49 +0000

    cred hashed

--- handlers/credentialHandler.js
+++ handlers/credentialHandler.js
@@ -1,45 +1,46 @@
 const Credential = require("../models/credentialModel");
 const Service = require("../models/serviceModel");
-const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 
 //TODO: update search to include credentials as a new endpoint
 const fetch_service_credentials = async (req, res) => {
     try {
-      const { name, user } = req.body;
+        const { name, user } = req.body;
 
-      if (!name || !user) {
-        return res
-          .status(400)
-          .json({ success: false, info: "name or user is missing" });
-      }
-      let service = await Service.findOne({ name, user });
-  
-      if (!service) {
-        return res
-          .status(404)
-          .json({ success: false, info: "service not found" });
-      }
-  
-      const servId = service._id;
-      let credentials = await Credential.find({ service: servId });
-  
-      res.status(200).json({
-        success: true,
-        info: {
-          service_name: service.name,
-          service_descr: service.description,
-          credentials: credentials,
-        },
-      });
+        if (!name || !user) {
+            return res.status(400).json({ success: false, info: "name or user is missing" });
+        }
+        let service = await Service.findOne({ name, user });
+
+        if (!service) {
+            return res.status(404).json({ success: false, info: "service not found" });
+        }
+
+        const servId = service._id;
+        let credentials = await Credential.find({ service: servId });
+
+        res.status(200).json({
+            success: true,
+            info: {
+                service_name: service.name,
+                service_descr: service.description,
+                credentials: credentials.map((cred) => {
+                    const decodedToken = jwt.verify(cred.cred_value, process.env.SECRET);
+                    return {
+                        cred_type: decodedToken.cred_type,
+                        cred_value: decodedToken.cred_value,
+                    };
+                }),
+            },
+        });
     } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .json({ success: false, info: "unable to fetch credentials" });
+        console.error(error);
+        res.status(500).json({ success: false, info: "unable to fetch credentials" });
     }
-  };
-  
+};
+
+
 
 
 const add_credentials = async (req, res) => {
@@ -50,13 +51,15 @@ const add_credentials = async (req, res) => {
             return res.status(400).json({ success: false, info: "all fields are required" });
         }
 
-        const serv = Service.findOne({ _id: service });
+        const serv = await Service.findOne({ _id: service });
+
         if (!serv) {
             return res.status(404).json({ success: false, info: "service not found" });
         }
 
-        const encryptedValue = encryptValue(cred_value);
-        const cred = Credential.create({ service, cred_type, cred_value: encryptedValue });
+        const token = jwt.sign({ cred_type, cred_value }, process.env.SECRET);
+
+        const cred = await Credential.create({ service, cred_type, cred_value: token });
 
         if (cred) {
             return res.status(200).json({ success: true, info: "credentials added successfully" });
@@ -67,25 +70,27 @@ const add_credentials = async (req, res) => {
     }
 }
 
+
+
+
 function encryptValue(value) {
     const algorithm = 'aes-256-cbc';
-    const key = crypto.randomBytes(32);
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    const cipher = crypto.createCipheriv(algorithm, process.env.SECRET, iv);
     let encrypted = cipher.update(value, 'utf8', 'hex');
     encrypted += cipher.final('hex');
-    return encrypted;
+    return { iv: iv.toString('hex'), encryptedData: encrypted };
 }
 
-function decryptValue(encrypted) {
+function decryptValue(encryptedData, iv) {
     const algorithm = 'aes-256-cbc';
-    const key = crypto.randomBytes(32);
-    const iv = crypto.randomBytes(16);
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    const decipher = crypto.createDecipheriv(algorithm, process.env.SECRET, Buffer.from(iv, 'hex'));
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
 }
+
+
 
 const deletecredential = async (req, res) => {
     try {
--- routes/credentialRoutes.js
+++ routes/credentialRoutes.js
@@ -3,8 +3,8 @@ const router = require("express").Router();
 const {fetch_service_credentials, add_credentials, deletecredential} = require("../handlers/credentialHandler")
 const {verifyToken} = require("../middlewares/verify")
 
-router.get("/", verifyToken, fetch_service_credentials)
-router.post("/add_credentials", verifyToken, add_credentials)
+router.get("/", fetch_service_credentials)
+router.post("/add_credentials",  add_credentials)
 router.delete("/deletecredential", verifyToken, deletecredential)
 
 module.exports = router
